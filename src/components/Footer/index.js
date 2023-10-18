104cc8510d74111e67fa169ebcb2196dea818596
Guifranso <guifranca60@gmail.com>
2023-10-18 03:19:09 -0300

    Implementação build

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,13 +3,15 @@ import ScrollToTop from '../ScrollToTop'
 
 export default function Footer () {
     return (
+        <>
         <div className="flex items-center dark:bg-back-primary-dark justify-around w-full h-20 py-16 px-14">
-            <ScrollToTop/>
             <div className="pl-12">
                 <i className="px-6 default-hover-item text-3xl fa-brands fa-github"></i>
                 <i className="px-6 default-hover-item text-3xl fa-brands fa-whatsapp"></i>
                 <i className="px-6 default-hover-item text-3xl fa-brands fa-linkedin"></i>
             </div>
         </div>
+        <ScrollToTop/>
+        </>
     )
 }
