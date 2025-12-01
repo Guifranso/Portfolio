d99d3b84147cd5b48854edfa7ff83247346f1e3c
Guilherme <guifranca60@gmail.com>
2025-12-01 17:36:57 -0300

    fix: header outline

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,9 +8,12 @@ export default function Footer() {
         <>
             <div className={styles.footer}>
                 <div className={styles.iconsContainer}>
-                    <i className={`default-hover-item fa-brands fa-github ${styles.icon}`}></i>
-                    <i className={`default-hover-item fa-brands fa-whatsapp ${styles.icon}`}></i>
-                    <i className={`default-hover-item fa-brands fa-linkedin ${styles.icon}`}></i>
+                    <a href="https://github.com/Guifranso" target="_blank" rel="noreferrer" className={`default-hover-item ${styles.icon}`}>
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/guilherme-fran%C3%A7a-a82150218/" target="_blank" rel="noreferrer" className={`default-hover-item ${styles.icon}`}>
+                        <i className="fa-brands fa-linkedin"></i>
+                    </a>
                 </div>
             </div>
             <ScrollToTop />
